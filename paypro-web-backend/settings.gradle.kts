675c6a4a590defa8c37a29251bf73d726f5cc3f3
AndrejAntonic <andrej.antonic2@gmail.com>
2023-11-22 18:52:21 +0100

    Initialised Spring Boot project.

---
+++ paypro-web-backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "paypro-web-backend"
---
+++ paypro-web-backend/src/test/java/air/found/payprowebbackend/PayproWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package air.found.payprowebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayproWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
