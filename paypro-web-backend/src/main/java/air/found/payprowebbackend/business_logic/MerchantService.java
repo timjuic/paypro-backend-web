cd46222a9407e90095f64ae63e4260061b621946
aantonic20 <aantonic20@foi.hr>
2023-12-27 10:07:59 +0100

    Implemented the controller code and created the service skeleton.

--- paypro-web-backend/src/main/java/air/found/payprowebbackend/business_logic/MerchantService.java
+++ paypro-web-backend/src/main/java/air/found/payprowebbackend/business_logic/MerchantService.java
@@ -1,7 +1,19 @@
 package air.found.payprowebbackend.business_logic;
 
+import air.found.payprowebbackend.core.ServiceResult;
+import air.found.payprowebbackend.core.models.Merchant;
+import air.found.payprowebbackend.data_access.persistance.MerchantRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class MerchantService {
+    private final MerchantRepository merchantsRepository;
+
+    public ServiceResult<List<Merchant>> getAllMerchants() {
+        return null;
+    }
 }
--- paypro-web-backend/src/main/java/air/found/payprowebbackend/endpoints/controllers/MerchantController.java
+++ paypro-web-backend/src/main/java/air/found/payprowebbackend/endpoints/controllers/MerchantController.java
@@ -1,13 +1,38 @@
 package air.found.payprowebbackend.endpoints.controllers;
 
 import air.found.payprowebbackend.business_logic.MerchantService;
+import air.found.payprowebbackend.core.ApiError;
+import air.found.payprowebbackend.core.ServiceResult;
+import air.found.payprowebbackend.core.models.Merchant;
+import air.found.payprowebbackend.core.network.ApiResponseBuilder;
+import air.found.payprowebbackend.core.network.ResponseBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/merchant")
 public class MerchantController {
     private final MerchantService merchantService;
+
+    @GetMapping
+    public ResponseEntity<ResponseBody<List<Merchant>>> getAllMerchants() {
+        ServiceResult<List<Merchant>> result = merchantService.getAllMerchants();
+        return respond(result, "Merchants fetched successfully");
+    }
+
+    private <T> ResponseEntity<ResponseBody<T>> respond(ServiceResult<T> result, String successMessage) {
+        if (result.isSuccess()) {
+            return ApiResponseBuilder.buildSuccessResponse(result.getData(), successMessage);
+        } else {
+            ApiError apiError = result.getApiError();
+            return ApiResponseBuilder.buildErrorResponse(HttpStatus.BAD_REQUEST, apiError.getErrorMessage(), apiError.getErrorCode(), apiError.getErrorName());
+        }
+    }
 }
