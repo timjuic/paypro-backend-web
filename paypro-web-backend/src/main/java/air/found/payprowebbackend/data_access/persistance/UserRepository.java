afe9d31134817459b2f408d4c22c5ca515b511dc
aantonic20 <aantonic20@foi.hr>
2024-01-02 10:18:17 +0100

    Added a new method of fetching data in user repository and created logic for generating a JWT token and a refresh token, as well as refreshing the token pair.

--- paypro-web-backend/src/main/java/air/found/payprowebbackend/data_access/persistance/UserRepository.java
+++ paypro-web-backend/src/main/java/air/found/payprowebbackend/data_access/persistance/UserRepository.java
@@ -3,5 +3,8 @@ package air.found.payprowebbackend.data_access.persistance;
 import air.found.payprowebbackend.core.models.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserAccount, Integer> {
+    Optional<UserAccount> findByEmailAddress(String email);
 }
